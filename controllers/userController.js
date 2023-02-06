e2c30e1e6d286c542bd17d294588b9bc1e195d87
DarioS <dsuarezlazarte@gmail.com>
2023-02-06 17:42:21 -0400

    Second commit - 28

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,23 @@
 const formLogin =  (req, res) => {
     res.render('auth/login', {
-
+        page: 'Log in'
     })
 };
 
 const formRegister =  (req, res) => {
-    res.render('auth/login', {
+    res.render('auth/register', {
+        page: 'Create account'
+    })
+};
 
+const formForgotPassword =  (req, res) => {
+    res.render('auth/forgot_password', { //esta es la dirección de la vista
+        page: 'Forgot Password'
     })
 };
 
 export {
     formLogin,
-    formRegister
+    formRegister,
+    formForgotPassword
 }
